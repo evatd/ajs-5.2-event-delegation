ba26c9e7bd6314b97faf9ea799133ed93e9cc2b6
Eva <eva.tkavc@gmail.com>
2017-11-20 20:21:14 +0000

    prettier code

--- exercise2.js
+++ exercise2.js
@@ -1,23 +1,24 @@
 // The element to attach the listener to
-var buttonWrapper = document.getElementById('button-wrapper');
+var buttonWrapper = document.getElementById("button-wrapper");
 
 // Add a listener to parent element... event will bubble up from children
-buttonWrapper.addEventListener('click', function (event) { // Pass in event object "event" as a parameter!
+buttonWrapper.addEventListener("click", function(event) {
+  // Pass in event object "event" as a parameter!
 
-    // event.target holds reference to clicked element.
-    // It is a DOM node, so all DOM methods, like .target, are available.
-    // We can copy it to local variable so we have less typing to do!
-    var clicked = event.target;
+  // event.target holds reference to clicked element.
+  // It is a DOM node, so all DOM methods, like .target, are available.
+  // We can copy it to local variable so we have less typing to do!
+  var clicked = event.target;
 
-    // Printing to see where the action comes from: e.g. clicking on button 12 prints <p class = "button" data-action="action 12">button 12</p>
-    console.log(event.target);
+  // Printing to see where the action comes from: e.g. clicking on button 12 prints <p class = "button" data-action="action 12">button 12</p>
+  console.log(event.target);
 
-    // Get data-action attribute from element
-    var data = clicked.getAttribute('data-action');
+  // Get data-action attribute from element
+  var data = clicked.getAttribute("data-action");
 
-    // Printing data: add a filter so we have something to work with!
-    // getAttribute returns "null" if attribute is not present on element.
-    if (data != null) {
-        console.log(data); //Clicking on button 12 prints 'action 12'
-    }
+  // Printing data: add a filter so we have something to work with!
+  // getAttribute returns "null" if attribute is not present on element.
+  if (data != null) {
+    console.log(data); //Clicking on button 12 prints 'action 12'
+  }
 });
